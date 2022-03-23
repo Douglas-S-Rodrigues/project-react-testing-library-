97bf590bd74a43d7138c95439188df03fb7f2a32
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-03-23 17:00:07 -0300

    tests for FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('testes para FavoritePokemons', () => {
+  it('deve testar se é exibido na tela a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+
+    const favorite = screen.getByText('No favorite pokemon found');
+    expect(favorite).toBeInTheDocument();
+  });
+
+  it('deve testar se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+
+    const pokeDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(pokeDetails);
+
+    const pokeFavoriteCheck = screen
+      .getByRole('checkbox', { name: /Pokémon favoritado/i });
+    userEvent.click(pokeFavoriteCheck);
+
+    const pokeFavorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(pokeFavorite);
+  });
+});
