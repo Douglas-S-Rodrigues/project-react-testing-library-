7d0c7519c7378a00e7f0ded9a9d9b24f6219b049
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-03-23 13:14:12 -0300

    tests for App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o App', () => {
+  it('Deveria exibir Home, About e Favorite Pokemon', () => {
+    const memoryHistory = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(home);
+    expect(memoryHistory.location.pathname).toBe('/');
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByRole('link', { name: /About/i });
+    userEvent.click(about);
+    expect(memoryHistory.location.pathname).toBe('/about');
+    expect(about).toBeInTheDocument();
+
+    const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favorites);
+    expect(memoryHistory.location.pathname).toBe('/favorites');
+    expect(favorites).toBeInTheDocument();
+  });
+  it('deve exibir pag notFond', () => {
+    const memoryHistory = renderWithRouter(<App />);
+    memoryHistory.push('/qualquercoisa');
+    const notFound = screen.getByRole('heading', { level: 2,
+      name: /Page requested not found/i});
+    expect(notFound).toBeInTheDocument();
+  });
+});
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import React from 'react';
+
+function renderWithRouter(component) {
+  const memoryHistory = createMemoryHistory();
+  render(
+    <Router history={ memoryHistory }>
+      { component }
+    </Router>
+  );
+  return memoryHistory;
+}
+
+export default renderWithRouter;
