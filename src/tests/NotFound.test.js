b36b10b1ebd741558b8e73fc40cca41e169377a0
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-03-23 17:32:04 -0300

    tests for NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('testes para notFound', () => {
+  it('testa se a pagina notFound contem um heading Page requested not found 😭', () => {
+    render(<NotFound />);
+
+    const text = screen.getByRole('heading', { name: /Page requested not found /i });
+    expect(text).toBeInTheDocument();
+  });
+
+  it('testa se a pagina notFound mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+
+    const img = screen.getAllByRole('img');
+    expect(img[1].src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    console.log(img[1]);
+  });
+});
