4c511aaf17e1514a556c6463a75269ddcf6049dc
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-03-29 18:42:39 -0300

    fix req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,26 +6,34 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testa o App', () => {
-  it('Deveria exibir Home, About e Favorite Pokemon', () => {
-    const memoryHistory = renderWithRouter(<App />);
+  it('Dever testar se exibi Home.', () => {
+    const { memoryHistory } = renderWithRouter(<App />);
+
     const home = screen.getByRole('link', { name: /Home/i });
     userEvent.click(home);
-    expect(memoryHistory.location.pathname).toBe('/');
-    expect(home).toBeInTheDocument();
+    const { location: { pathname } } = memoryHistory;
+    expect(pathname).toBe('/');
+  });
+  it('deve testar o link About', () => {
+    const { memoryHistory } = renderWithRouter(<App />);
 
     const about = screen.getByRole('link', { name: /About/i });
     userEvent.click(about);
-    expect(memoryHistory.location.pathname).toBe('/about');
-    expect(about).toBeInTheDocument();
-
-    const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
-    userEvent.click(favorites);
-    expect(memoryHistory.location.pathname).toBe('/favorites');
-    expect(favorites).toBeInTheDocument();
+    const { location: { pathname } } = memoryHistory;
+    expect(pathname).toBe('/about');
   });
-  it('deve exibir pag notFond', () => {
-    const memoryHistory = renderWithRouter(<App />);
-    memoryHistory.push('/qualquercoisa');
+  it('deve testar o link Favorites"', () => {
+    const { memoryHistory } = renderWithRouter(<App />);
+
+    const favorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favorites);
+    const { location: { pathname } } = memoryHistory;
+    expect(pathname).toBe('/favorites');
+  });
+  it('deve testar se o link estiver errado é levado ao NotFound', () => {
+    const { memoryHistory } = renderWithRouter(<App />);
+
+    memoryHistory.push('/paginanãoexistente');
     const notFound = screen.getByRole('heading', { level: 2,
       name: /Page requested not found/i });
     expect(notFound).toBeInTheDocument();
